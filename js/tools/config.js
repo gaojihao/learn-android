f2260a21b2c31649e74e7179a24d1ceafdc0c8fd
gaojihao <lizhi1026@126.com>
2025-10-13 14:53:32 +0800

    update

--- js/tools/config.js
+++
@@ -1,8 +0,0 @@
-import {PixelRatio,Dimensions} from 'react-native';
-
-export default {
-    screenWidth:Dimensions.get('window').width,
-    screenHeight:Dimensions.get('window').height,
-    themeColor: '#167DF7',
-    onePix:1/PixelRatio.get(),
-};
